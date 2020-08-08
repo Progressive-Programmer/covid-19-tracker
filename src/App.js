71a7317f642d664a41ddf07839b24196c81c8561
Aman <v4vendetta5v@gmail.com>
2020-08-08 06:50:50 +0530

    All unsorted API data added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import React, { useState, useEffect } from 'react';
-import {
-    MenuItem,
-    FormControl,
-    Select
-} from "@material-ui/core";
+import { Card, MenuItem, FormControl, Select, CardContent } from "@material-ui/core";
+import InfoBox from './InfoBox';
+import Map from './Map';
+import Table from './Table';
 import './App.css';
 
 function App() {
-    const [countries, setCountries] = useState([
-        'USA', 'INDIA'
-    ]);
+    const [countries, setCountries] = useState([]);
+    const [country, setCountry] = useState('worldwide');
+    const [countryInfo, setCountryInfo] = useState({});
+    const [tableData, setTableData] = useState([]);
 
 // STATE = the way to write variables in react
 
 // USEEFFECT = Runa a piece of code based on a given condition
+    useEffect(() => {
+        fetch("https://disease.sh/v3/covid-19/all")
+        .then(response => response.json())
+        .then((data) => {
+            setCountryInfo(data);
+        })
+    }, [])
+
+
+
 
     useEffect(() => {
         // async -> send a request, wait for it , do something wiht it 
@@ -27,7 +37,8 @@ function App() {
                         name: country.country,          //India, United Stated, United Kingdon.....
                         value: country.countryInfo.iso2,       //UK, USA, IN
                     }));
-            
+                    
+                    setTableData(data);
                     setCountries(countries);
                 });
 
@@ -36,36 +47,62 @@ function App() {
     }, []);
 
 
+    const onCountryChange = async (event) => {
+        const countryCode = event.target.value;
+
+        console.log("Youooo>>>>>", countryCode);
+        setCountry(countryCode);
+
+        const url = countryCode === 'worldwide' 
+            ? 'https://disease.sh/v3/covid-19/all' 
+            : `https://disease.sh/v3/covid-19/countries/${countryCode}`
+         
+            await fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                setCountry(countryCode);
+                //all of the date... from the country response
+                setCountryInfo(data);
+        })
+    };
+
+
+    console.log('COUNTRY INFO>>>>', countryInfo)
     return ( 
         <div className = "app" >
-        <div className="app__header">
-                                
-            <h1 > COVID 19 TRACKER </h1> 
-            <FormControl className = "app__dropdown">
-                <Select variant = "outlined" value = "abc" >
+            <div className="app__left">  
+                <div className="app__header">                   
+                    <h1 > COVID 19 TRACKER </h1> 
+                    <FormControl className = "app__dropdown">
+                        <Select variant = "outlined" onChange= {onCountryChange} value = {country} >
+                            <MenuItem value = "worldwide">Worldwide</MenuItem>
+                            {countries.map(country => (
+                                    <MenuItem value = {country.value}>{country.name }</MenuItem> 
+                                ))}
+                        
+                        </Select>        
+                    </FormControl>                    
+                </div>                        
+                    
+                <div className="app__stats">
+                    <InfoBox title="Coronavirus Cases" cases={countryInfo.todayCases} total={countryInfo.cases}/>
 
-                    {
-                        countries.map(country => (
-                            <MenuItem value = {country.value}>{country.name}</MenuItem> 
-                        ))
-                    }
+                    <InfoBox title="Recovered" cases={countryInfo.todayRecovered} total={countryInfo.recovered} />
+                    
+                    <InfoBox title="Deaths" cases={countryInfo.todayDeaths} total={countryInfo.deaths} />
+                </div>
 
-                    {/* <MenuItem value = "worldwide">Worldwide</MenuItem> 
-                    <MenuItem value = "worldwide">Option one</MenuItem> 
-                    <MenuItem value = "worldwide">Option two</MenuItem> 
-                    <MenuItem value = "worldwide">Option three</MenuItem>         */}
-                </Select>        
-            </FormControl>
-        </div>
+                <Map />
+                
+            </div>
 
-        { /* Head */ } { /* title + input drop */ }
-
-        { /* InfoBox */ } { /* InfoBox */ } { /* InfoBox */ }
-
-
-        { /* Table */ } { /* Graph */ }
-
-        { /* Map */ } 
+            <Card className="app__right">
+                <CardContent>
+                    <h3>I am a card onm right side</h3>
+                    <Table countries={tableData} />
+                    <h3>World wide new cases</h3>
+                </CardContent>
+            </Card>
         </div>
     );
 }
